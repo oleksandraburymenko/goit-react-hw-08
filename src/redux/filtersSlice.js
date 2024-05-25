e8e4be4dbb00889315334a92c1e134176192a214
oleksandraburymenko <115561772+oleksandraburymenko@users.noreply.github.com>
2024-05-25 16:33:51 +0300

    add files

---
+++ src/redux/filtersSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const slice = createSlice({
+  name: "filters",
+  initialState: {
+    value: "",
+  },
+  reducers: {
+    setFilter(state, action) {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const selectValueFilters = (state) => state.filters.value;
+
+export const { setFilter } = slice.actions;
+
+export default slice.reducer;
